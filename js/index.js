025b6a660ea088e058dec42144532354092dc273
jonathanteasdale <jonathant94@protonmail.com>
2023-08-08 22:47:55 +0100

    js animation test 3

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const the_animation = document.querySelectorAll('.animation')
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('scroll-animations')
+            entry.target.classList.add('scroll-animation')
         }
             else {
                 entry.target.classList.remove('scroll-animation')
@@ -11,6 +11,14 @@ const observer = new IntersectionObserver((entries) => {
 
     })
 },
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  }
 
 
 
