5d793040397b6674997e09d178442f0afd7a5d0f
jonathanteasdale <jonathant94@protonmail.com>
2023-08-08 22:08:26 +0100

    javascript scroll animation test 1

---
+++ js/index.js
@@ -0,0 +1,46 @@
+window.addEventListener('scroll', function() {
+  var content = document.querySelector('.slide-in');
+  var contentPosition = content.getBoundingClientRect().top;
+  var screenHeight = window.innerHeight;
+
+  if (contentPosition < screenHeight) {
+    content.classList.add('slide-in-active');
+  }
+});
+
+
+
+/*
+const slideIn = document.querySelectorAll('.slide-in');
+
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide() {
+  slideIn.forEach(slide => {
+    const slideInAt = (window.scrollY + window.innerHeight) - slide.height / 2;
+    const imageBottom = slide.offsetTop + slide.height;
+    const isHalfShown = slideInAt > slide.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+*/
